5686c05953a239d84df845916b9b76af01dfabfe
jsloman <JonathanS2@gmail.com>
2016-03-29 23:05:39 +0100

    more api-ing

--- src/main/java/com/guidentifier/api/GuidentifierApi.java
+++ src/main/java/com/guidentifier/api/GuidentifierApi.java
@@ -1,21 +1,41 @@
 package com.guidentifier.api;
 
+import java.util.List;
+
+import javax.inject.Named;
+
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
-import javax.inject.Named;
+import com.guidentifier.dao.DAO;
 import com.guidentifier.model.Type;
 
 /** An endpoint class we are exposing */
-@Api(name = "myApi", version = "v1", namespace = @ApiNamespace(ownerDomain = "guidentifier.com", ownerName = "guidentifier.com", packagePath = ""))
+@Api(name = "myApi", version = "v1", namespace = @ApiNamespace(ownerDomain = "guidentifier.com",
+		ownerName = "guidentifier.com", packagePath = ""))
 public class GuidentifierApi {
+	DAO dao;
+	
+	public GuidentifierApi()
+	{
+		dao = new DAO();
+	}
 
 	/** A simple endpoint method that takes a name and says Hi back */
 	@ApiMethod(name = "sayHi")
 	public Type sayHi(@Named("name") String name) {
-		Type response = new Type("test-type");
+		Type response = new Type("test-type" + name);
 		response.setId(1L);
 		return response;
+	}
+	
+	public Type addType(Type type) {
+		dao.add(type);
+		return type;
+	}
+	
+	public List<Type> getTypes() {
+		return dao.getTypes();
 	}
 
 }
