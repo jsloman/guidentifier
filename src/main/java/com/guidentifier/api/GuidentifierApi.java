57c30bd742edd6f6a6c004408d40992c7f6731e6
jsloman <JonathanS2@gmail.com>
2016-03-30 23:04:28 +0100

    got webapp working a bit.

--- src/main/java/com/guidentifier/api/GuidentifierApi.java
+++ src/main/java/com/guidentifier/api/GuidentifierApi.java
@@ -2,16 +2,13 @@ package com.guidentifier.api;
 
 import java.util.List;
 
-import javax.inject.Named;
-
 import com.google.api.server.spi.config.Api;
-import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 import com.guidentifier.dao.DAO;
 import com.guidentifier.model.Type;
 
 /** An endpoint class we are exposing */
-@Api(name = "myApi", version = "v1", namespace = @ApiNamespace(ownerDomain = "guidentifier.com",
+@Api(name = "guidentifierApi", version = "v1", namespace = @ApiNamespace(ownerDomain = "guidentifier.com",
 		ownerName = "guidentifier.com", packagePath = ""))
 public class GuidentifierApi {
 	DAO dao;
@@ -19,14 +16,6 @@ public class GuidentifierApi {
 	public GuidentifierApi()
 	{
 		dao = new DAO();
-	}
-
-	/** A simple endpoint method that takes a name and says Hi back */
-	@ApiMethod(name = "sayHi")
-	public Type sayHi(@Named("name") String name) {
-		Type response = new Type("test-type" + name);
-		response.setId(1L);
-		return response;
 	}
 	
 	public Type addType(Type type) {
