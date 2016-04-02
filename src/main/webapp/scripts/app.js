7a8cfecfcc60132b4a3e10fa9b1ec4e63b539340
jsloman <JonathanS2@gmail.com>
2016-04-02 23:05:21 +0100

    working with saving type

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -1,77 +1,104 @@
-var data = [
-  {id: 1, author: "Pete Hunt", text: "This is one comment"},
-  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-];
+var Type = React.createClass({
+	rawMarkup: function() {
+		var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+		return { __html: rawMarkup };
+	},
 
-var Comment = React.createClass({
-  rawMarkup: function() {
-    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
-    return { __html: rawMarkup };
-  },
-
-  render: function() {
-    return (
-      <div className="comment">
-        <h2 className="commentAuthor">
-          {this.props.author}
-        </h2>
-        <span dangerouslySetInnerHTML={this.rawMarkup()} />
-      </div>
-    );
-  }
+	render: function() {
+		return (
+				<div className="type">
+					<h2 className="typeName">
+						{this.props.name}
+					</h2>
+				</div>
+		);
+	}
 });
 
-var CommentList = React.createClass({
-	  render: function() {
-	    var commentNodes = this.props.data.map(function(comment) {
+var TypeList = React.createClass({
+	render: function() {
+	    var typeNodes = this.props.data.map(function(type) {
 	      return (
-	        <Comment author={comment.id} key={comment.id}>
-	          {comment.name}
-	        </Comment>
+	        <Type key={type.id} name={type.name}>
+	          {type.name}
+	        </Type>
 	      );
 	    });
 	    return (
-	      <div className="commentList">
-	        {commentNodes}
+	      <div className="typeList">
+	        {typeNodes}
 	      </div>
 	    );
-	  }
-	});
+	}
+});
 
-var CommentForm = React.createClass({
-  render: function() {
-    return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
-    );
-  }
+var TypeForm = React.createClass({
+	getInitialState: function() {
+	  return {name: ''};
+	},
+	handleNameChange: function(e) {
+	  this.setState({name: e.target.value});
+	},
+	handleSubmit: function(e) {
+		e.preventDefault();
+		var name = this.state.name.trim();
+		if (!name) {
+			return;
+		}
+	    this.props.onCommentSubmit({name: name});
+		this.setState({name: ''});
+	},
+	render: function() {
+		return (
+	      <form className="commentForm" onSubmit={this.handleSubmit}>
+	        <input
+	          type="text"
+	          placeholder="Type name"
+	          value={this.state.name}
+	          onChange={this.handleNameChange}
+	        />
+	        <input type="submit" value="Post" />
+	      </form>
+	    );
+	}
 });
 
 
-var CommentBox = React.createClass({
-	  getInitialState: function() {
-	    return {data: []};
-	  },
-	  componentDidMount: function() {
-			gapi.client.guidentifierApi.guidentifierApi.getTypes().execute(
-					function(mydata){ 
-						console.log('got data: ' + JSON.stringify(mydata)); 
-						this.setState({data: mydata.items})
-					}.bind(this)
-			);
-		  },
-	  render: function() {
+var TypeBox = React.createClass({
+	loadCommentsFromServer: function() {
+		gapi.client.guidentifierApi.guidentifierApi.getTypes().execute(
+			function(mydata) { 
+				console.log('got data: ' + JSON.stringify(mydata)); 
+				this.setState({data: mydata.items})
+			}.bind(this)
+		);
+	},
+	handleTypeSubmit: function(typeObj) {
+		console.log(' adding type: ' + typeObj + ' which is: ' + JSON.stringify(typeObj));
+	    gapi.client.guidentifierApi.guidentifierApi.addType(typeObj).execute(
+			   function(mydata) {
+				   console.log('added type: ' + JSON.stringify(mydata));
+				   this.loadCommentsFromServer();
+			   }.bind(this)
+	   );
+	},
+	getInitialState: function() {
+		return {data: []};
+	},
+	componentDidMount: function() {
+	    this.loadCommentsFromServer();
+	},
+	render: function() {
 	    return (
-	      <div className="commentBox">
+	      <div className="typeBox">
 	        <h1>Comments</h1>
-	        <CommentList data={this.state.data} />
-	        <CommentForm />
+	        <TypeList data={this.state.data} />
+	        <TypeForm onCommentSubmit={this.handleTypeSubmit} />
 	      </div>
 	    );
-	  }
-	});
+	}
+});
 
 // add it to global context so we can reference this from javascript.
-window.CommentBox = CommentBox;
+window.TypeBox = TypeBox;
 
